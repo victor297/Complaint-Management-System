0266f70b7538362979eb27e325ca2857c46c5fd3
david victor <38535954+victor297@users.noreply.github.com>
2024-08-26 10:49:03 +0100

    Update Config.js

--- src/Components/Firebase/Config.js
+++ src/Components/Firebase/Config.js
@@ -3,13 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyAXM3g_jSCfFH56MCCwyNpwQa6u19FUvO0",
-  authDomain: "alpha-cms-524ce.firebaseapp.com",
-  projectId: "alpha-cms-524ce",
-  storageBucket: "alpha-cms-524ce.appspot.com",
-  messagingSenderId: "735554409625",
-  appId: "1:735554409625:web:7bdd8f325bc66bb7f5d408",
+   apiKey: "AIzaSyDHyHnKHRFGrJ-gTy45vDATmOpg_9RjteI",
+  authDomain: "compliant-7a165.firebaseapp.com",
+  projectId: "compliant-7a165",
+  storageBucket: "compliant-7a165.appspot.com",
+  messagingSenderId: "769768032732",
+  appId: "1:769768032732:web:8ec97ca6268bd680e2e719",
 };
+
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
